8285a3ffa37c94c0567f9f32e53ea14ecd184e62
ubeslug <136268381+ubeslug@users.noreply.github.com>
2025-02-12 09:27:16 -0800

    stitched together encounters and temp backgrounds

--- scripts/title.gd
+++ scripts/title.gd
@@ -45,4 +45,4 @@ func _on_no_quit_pressed() -> void:
 func _on_new_game_pressed():
 	fade_handler.play("fade-out")
 	await fade_handler.animation_finished
-	get_tree().change_scene_to_file("res://scenes/playtest scenes/intro.tscn")
+	get_tree().change_scene_to_file("res://scenes/encounters/berry_encounter.tscn")
